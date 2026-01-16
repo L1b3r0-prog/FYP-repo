48127f7b908727f5a796bb1285c5596f26df9b7a
L1b3r0-prog <leebpk@gmail.com>
2026-01-16 23:13:50 +0800

    adding

---
+++ members.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router";
+
+export default function Members() {
+    return (
+        <div>
+        <h1>This is the members page</h1>
+        <Link to="/">Back to homepage</Link>
+    </div>
+    )
+}
