bca91c89f7de7c20ab908a78257afac84f27a0de
L1b3r0-prog <leebpk@gmail.com>
2026-01-16 23:12:35 +0800

    clearing

--- fyp-project/app/routes.ts
+++
@@ -1,5 +0,0 @@
-import { type RouteConfig, index, route} from "@react-router/dev/routes";
-
-export default [index("routes/homepage.tsx"), 
-                route("members", "routes/members.tsx")] satisfies RouteConfig;
-
