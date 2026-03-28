a084415e0643f59d7b8986665d43ce5bb46f2f16
L1b3r0-prog <leebpk@gmail.com>
2026-03-28 10:48:20 +0800

    Update navbar.tsx

--- fyp-project/app/components/navbar.tsx
+++ fyp-project/app/components/navbar.tsx
@@ -3,7 +3,7 @@ import { NavLink, useNavigate, useLocation } from "react-router";
 export default function Navbar() {
     return (
         <header className="px-15">
-            <div className="container flex items-center justify-between py-4 mx-10">
+            <div className="container flex items-center justify-between py-4 mx-auto">
                 {/* Logo button to go back to Home */}
                 <NavLink to="/" className="flex-shrink-0">
                     <span className="text-green-500 font-bold text-5xl">
@@ -12,7 +12,7 @@ export default function Navbar() {
                 </NavLink>
 
                 { /* This is all the buttons */ }
-                <div className="flex items-center pl-300 ml-auto">
+                <div className="flex items-center ml-auto">
                     {/* Home, Team and Document button */}
                     <nav className="flex items-center">
                         <NavLink to="/" className="text-black hover:text-green-500 text-lg px-4 py-2">
